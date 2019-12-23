36f26b9a14021b9bad7920e51d0ef9845cf139cb
Sriram Thiagarajan <sri.sjc@gmail.com>
2019-12-23 17:10:36 +0530

    format gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,9 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/`,
-      },
+        path: `${__dirname}/src/data/`
+      }
     },
-    `gatsby-transformer-csv`,
-  ],
-}
+    `gatsby-transformer-csv`
+  ]
+};
