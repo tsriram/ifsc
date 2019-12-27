d763b763270a20bc51ec303afee4c91e8d7e8689
Sriram Thiagarajan <sri.sjc@gmail.com>
2019-12-27 16:32:20 +0530

    add navbar to layout component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import React from "react";
 
 import "../styles/index.scss";
@@ -6,6 +7,23 @@ interface LayoutProps {
   readonly children: JSX.Element;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => <main>{children}</main>;
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <nav
+        className="navbar is-fixed-top is-primary"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            IƒSC
+          </Link>
+        </div>
+      </nav>
+      <main>{children}</main>
+    </div>
+  );
+};
 
 export default Layout;
