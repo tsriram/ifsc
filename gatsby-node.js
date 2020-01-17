8e4ca4d2574d9598eb472347f2b348af9697c5c7
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-01-17 14:53:56 +0530

    some logging + time stats in gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,7 +21,11 @@ exports.onCreateNode = ({ node, actions }) => {
   }
 };
 
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const progress = reporter.createProgress(`ifsc/gatsby-node.js`);
+  console.time("(ifsc) total exports.createPages");
+  console.time("(ifsc) initial graphql query");
+  progress.setStatus("initial graphl query");
   const { createPage } = actions;
   const result = await graphql(`
     query {
@@ -37,6 +41,18 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `);
+
+  console.timeEnd("(ifsc) initial graphql query");
+
+  console.time("(ifsc) created pages");
+
+  progress.start();
+  progress.total = result.data.allIfscCsv.edges.length - 1;
+  let start = Date.now();
+  progress.setStatus(
+    "Calling createPage for " + result.data.allIfscCsv.edges.length + " pages"
+  );
+
   result.data.allIfscCsv.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
@@ -45,5 +61,19 @@ exports.createPages = async ({ graphql, actions }) => {
         id: node.id
       }
     });
+    progress.tick(1);
   });
+
+  progress.setStatus(
+    "Called createPage for " +
+      (result.data.allIfscCsv.edges.length - 1) +
+      " pages at " +
+      (result.data.allIfscCsv.edges.length - 1) /
+        ((Date.now() - start) / 1000) +
+      " pages/s"
+  );
+  progress.done();
+  console.timeEnd("(ifsc) created pages");
+  console.timeEnd("(ifsc) total exports.createPages");
+  progress.setStatus("createPages finished");
 };
