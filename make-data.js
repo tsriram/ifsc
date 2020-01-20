47d141f0fd977de2dccbedf0a4ee397327f843cf
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-01-20 06:46:24 +0530

    script to transform raw data to the desired format
    For the data to work with gatsby-transformer-json, this script reads each JSON file and write it as an array

---
+++ make-data.js
@@ -0,0 +1,30 @@
+const flatMap = require("lodash/flatMap");
+const path = require("path");
+const fs = require("fs");
+
+const originalDataPath = path.join(__dirname, "src", "data", "original");
+
+const developDataPath = path.join(__dirname, "src", "data", "develop");
+fs.mkdirSync(developDataPath);
+
+const buildDataPath = path.join(__dirname, "src", "data", "build");
+fs.mkdirSync(buildDataPath);
+
+try {
+  const files = fs.readdirSync(originalDataPath);
+
+  for (const file of files) {
+    const readFilePath = path.join(originalDataPath, file);
+    const developFilePath = path.join(developDataPath, file);
+    const buildFilePath = path.join(buildDataPath, file);
+
+    const fileContent = fs.readFileSync(readFilePath);
+    const jsonContent = JSON.parse(fileContent);
+    const contentArray = flatMap(jsonContent);
+
+    fs.writeFileSync(buildFilePath, JSON.stringify(contentArray));
+    fs.writeFileSync(developFilePath, JSON.stringify(contentArray.slice(0, 1)));
+  }
+} catch (error) {
+  console.error(`Error while reading files: ${error}`);
+}
