14d8a23cd48f4a7a1cf4a3a28eb0d77362bdf7de
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-01-23 11:24:28 +0530

    home page to show list of banks
    This will later be converted to links that shows list of states for each bank, that links to cities etc.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,57 @@
 import Layout from "../components/layout";
+import { graphql } from "gatsby";
 import React from "react";
 
-const Index: React.FC<{}> = () => {
+interface Node {
+  readonly node: {
+    readonly BANK: string;
+  };
+}
+
+interface HomePageProps {
+  readonly data: {
+    readonly allIfscJson: {
+      readonly edges: ReadonlyArray<Node>;
+    };
+  };
+}
+
+const HomePage: React.FC<HomePageProps> = ({ data }) => {
+  const { edges } = data.allIfscJson;
   return (
     <Layout>
-      <div>Hello world!</div>
+      <React.Fragment>
+        <h1 className="title">Banks</h1>
+        <div className="columns is-multiline is-mobile is-centered">
+          {edges.map(edge => {
+            const { BANK: bank } = edge.node;
+            return (
+              <div
+                className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd"
+                key={bank}
+              >
+                <div className="card">
+                  <div className="card-content">{bank}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </React.Fragment>
     </Layout>
   );
 };
 
-export default Index;
+export const query = graphql`
+  query {
+    allIfscJson {
+      edges {
+        node {
+          BANK
+        }
+      }
+    }
+  }
+`;
+
+export default HomePage;
