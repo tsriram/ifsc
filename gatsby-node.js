81a56557596d5ef167c2a6b26f92a9d6e7b641ea
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-02-14 14:44:50 +0530

    clean up gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,64 +1,4 @@
-const slugify = require("slugify");
 const path = require("path");
-
-const slugifyOptions = {
-  lower: true,
-  remove: /[^a-zA-Z\d\s]/g
-};
-
-// exports.onCreateNode = ({ node, actions }) => {
-//   if (node.internal.type === "IfscJson") {
-//     const { createNodeField } = actions;
-//     const { BANK: bank, BRANCH: branch, CITY: city, STATE: state } = node;
-
-//     const bankSlug = bank && slugify(bank, slugifyOptions);
-//     const stateSlug = state && slugify(state, slugifyOptions);
-//     const citySlug = city && slugify(city, slugifyOptions);
-//     const branchSlug = branch && slugify(branch, slugifyOptions);
-
-//     if (!(bankSlug && stateSlug && citySlug && branchSlug)) {
-//       return;
-//     }
-
-//     let slug = "";
-//     if (bankSlug) {
-//       slug += bankSlug;
-//     }
-//     createNodeField({
-//       node,
-//       name: `bankSlug`,
-//       value: bankSlug
-//     });
-
-//     if (stateSlug) {
-//       slug += `/${stateSlug}`;
-//       createNodeField({
-//         node,
-//         name: `stateSlug`,
-//         value: stateSlug
-//       });
-//     }
-
-//     if (citySlug) {
-//       slug += `/${citySlug}`;
-//       createNodeField({
-//         node,
-//         name: `citySlug`,
-//         value: citySlug
-//       });
-//     }
-
-//     if (branchSlug) {
-//       slug += `/${branchSlug}-branch`;
-//     }
-
-//     createNodeField({
-//       node,
-//       name: `slug`,
-//       value: slug
-//     });
-//   }
-// };
 
 // This will create pages that enable navigating from home page (with list of banks)
 //  --> bank-state page (list of states that the bank is in) --> bank-state-city page
@@ -66,9 +6,6 @@ const slugifyOptions = {
 // --> individual branch page (which has the acutal IFSC)
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  // const bankPages = {};
-  // const bankStatePages = {};
-  // const bankStateCityPages = {};
 
   const allIfsc = await graphql(`
     query {
@@ -97,12 +34,6 @@ exports.createPages = async ({ graphql, actions }) => {
           id: node.id
         }
       });
-    } else {
-      console.log("NOT ALL SLUGS AVAILABLE: ");
-      // console.log("branchSlug: ", branchSlug);
-      // console.log("citySlug: ", citySlug);
-      // console.log("stateSlug: ", stateSlug);
-      // console.log("bankSlug: ", bankSlug);
     }
   });
 
@@ -182,40 +113,4 @@ exports.createPages = async ({ graphql, actions }) => {
       });
     }
   );
-
-  // if (!bankPages[bankSlug]) {
-  //   createPage({
-  //     path: bankSlug,
-  //     component: path.resolve(`./src/templates/bank-page.tsx`),
-  //     context: {
-  //       bankSlug
-  //     }
-  //   });
-  //   bankPages[bankSlug] = true;
-  // }
-
-  // if (!bankStatePages[bankStatePagePath]) {
-  //   createPage({
-  //     path: bankStatePagePath,
-  //     component: path.resolve(`./src/templates/bank-state-page.tsx`),
-  //     context: {
-  //       bankSlug: bankSlug,
-  //       stateSlug: stateSlug
-  //     }
-  //   });
-  //   bankStatePages[bankStatePagePath] = true;
-  // }
-
-  // if (!bankStateCityPages[bankStateCityPagePath]) {
-  //   createPage({
-  //     path: bankStateCityPagePath,
-  //     component: path.resolve(`./src/templates/bank-state-city-page.tsx`),
-  //     context: {
-  //       bankSlug: bankSlug,
-  //       stateSlug: stateSlug,
-  //       citySlug: citySlug
-  //     }
-  //   });
-  //   bankStateCityPages[bankStateCityPagePath] = true;
-  // }
 };
